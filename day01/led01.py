b8c0e7ca798e873188395ddfd3ad82ccea000d4e
JinyongSon <wlsdydths@naver.com>
2024-06-20 14:57:06 +0900

    1일차

---
+++ day01/led01.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import time
+ 
+led1 = 21
+led2 = 20
+led3 = 16
+ 
+#GPIO를 BCM모드로 설정
+GPIO.setmode(GPIO.BCM)
+#GPIO핀 설정(입력/출력)
+GPIO.setup(led1, GPIO.OUT)
+GPIO.setup(led2, GPIO.OUT)
+GPIO.setup(led3, GPIO.OUT)
+
+try:
+    while True:
+        GPIO.output(led1, True)
+        GPIO.output(led2, True)
+        GPIO.output(led3, True)
+        time.sleep(1)
+        GPIO.output(led1, False)
+        GPIO.output(led2, True)
+        GPIO.output(led3, False)
+        time.sleep(1)
+        GPIO.output(led1, False)
+        GPIO.output(led2, False)
+        GPIO.output(led3, True)
+        time.sleep(1)
+
+except KeyboardInterrupt:  #Ctrl + c
+    GPIO.cleanup()
