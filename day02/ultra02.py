7e4c5d7e1bdc04e196afe14c778b80588a714283
JinyongSon <wlsdydths@naver.com>
2024-06-28 17:26:13 +0900

    6일차 업데이트

--- day02/ultra02.py
+++ day02/ultra02.py
@@ -3,19 +3,19 @@ import RPi.GPIO as GPIO
 import time
 
 def measure():
-	GPIO.output(trigPin, True)	# 10us 동안 high레벨로  triger출력하여 초음파발생 준비
+	GPIO.output(trigPin, True)	
 	time.sleep(0.00001)
 	GPIO.output(trigPin, False)
-	start = time.time()						# 현재시간 저장
+	start = time.time()			
 
-	while GPIO.input(echoPin) == False:	# echo가 없으면
-		start = time.time()								# 현재 시간을 start변수에 저장하고
-	while GPIO.input(echoPin) == True:	# echo가 있으면
-		stop = time.time()								# 현재시간을 stop변수에 저장
-	elapsed = stop - start							# 걸린 시간을 구하고
-	distance = (elapsed * 19000) / 2		# 초음파속도를 이용해서 거리계산
-
-	return distance											# 거리반환
+	while GPIO.input(echoPin) == False:
+		start = time.time()				
+	while GPIO.input(echoPin) == True:	
+		stop = time.time()				
+	elapsed = stop - start				
+	distance = (elapsed * 19000) / 2	
+	
+	return distance						
 
 # 핀설정
 piezoPin = 13
